0a235f622e5f8bed785fcbf8d31df96abd08ce73
pterpmnta <pedro.pimienta.morales@gmail.com>
2022-10-01 16:59:03 -0500

    env variables config seted

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/auth.route";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
