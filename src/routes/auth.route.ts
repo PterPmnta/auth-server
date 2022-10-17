db60466064cab00b833715a5eb768ae94e4cac32
pterpmnta <pedro.pimienta.morales@gmail.com>
2022-10-17 15:16:09 -0500

    middleware and config validator added

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,14 +1,26 @@
 import { Router } from "express";
+import {check} from "express-validator"
 import {
   createUser,
   loginUser,
   validateJWT,
 } from "../controller/auth.controller";
+import { fieldValidate } from "../middlewares/field-validate";
 
 const routes = Router();
 
-routes.post('/new', createUser);
-routes.get('/', loginUser);
+routes.post('/new', 
+            check('name', 'El nombre es obligatorio').not().isEmpty(),     
+            check('email', 'El correo es obligatorio').isEmail(),      
+            check('password', 'La contraseña es obligatoria. Minimo de 6 caracteres').isLength({min: 6}),     
+            fieldValidate, 
+            createUser);
+
+routes.get('/', 
+          check('email', 'El correo es obligatorio').isEmail(),      
+          check('password', 'La contraseña es obligatoria. Minimo de 6 caracteres').isLength({min: 6}),
+          fieldValidate, 
+          loginUser);
 routes.get('/renew', validateJWT);
 
 export default routes;
