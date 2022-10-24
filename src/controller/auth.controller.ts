39f783c61e0666e1efefe0287529ef5e38ba15bd
pterpmnta <pedro.pimienta.morales@gmail.com>
2022-10-23 20:43:55 -0500

    user creation process and email validation made it

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,14 +1,39 @@
 import { Request, Response } from "express";
+import Usuario from "../models/usuario.model";
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async(req: Request, res: Response) => {
   try {
-    console.log(req.body);
+
+    const {email, name, password} = req.body
+
+    let usuario = await Usuario.findOne({email});
+
+    if(usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo que intenta usar ya esta registrado.'
+      });
+    }
+
+    const dbUser = new Usuario(req.body);
+    dbUser.save();
+
+    return res.status(201).json({
+      ok: true, 
+      uid: dbUser.id,
+      name
+    })
+
     return res.json({
       ok: true,
       msg: "Crear usuario",
     });
   } catch (error) {
-    return error;
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
   }
 };
 
