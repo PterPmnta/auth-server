c3a45c27d9f4c12d33a91be9168bbc1c35987501
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-01-24 17:22:18 -0500

    remove console log code

--- src/db/config.db.ts
+++ src/db/config.db.ts
@@ -5,8 +5,6 @@ dotenv.config();
 export const  dbConnection = async() => {
     try {
         const db_connection: string = process.env.MONGODB_CONNECTION!;
-        console.log(db_connection);
-        console.log(typeof db_connection);
         await mongoose.connect(db_connection)
         console.log("DB Online");
     } catch (error) {
